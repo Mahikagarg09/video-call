10a1f960adc2843df17da7054c1ddc554b8c2dcc
Mahikagarg09 <mahika_garg@yahoo.com>
2023-03-30 17:57:04 +0530

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import {Route,Routes,Navugate} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Room from './pages/Room';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/room/:roomID" element={<Room />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
