5322c2f2dfdf1bd18f8fa03c983801522b9b6d28
Mahikagarg09 <mahika_garg@yahoo.com>
2023-03-30 18:43:39 +0530

    add ZegoUIKitPrebuilt

--- src/pages/Room.js
+++ src/pages/Room.js
@@ -1,9 +1,45 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 
 export default function Room() {
-  return (
-    <div>
-      <h1>Room</h1>
-    </div>
-  );
+    const { roomID } = useParams();
+    console.log(roomID);
+
+    const meeting = async (element) => {
+        const appID = 78778157;
+        const serverSecret = "91dfb27095046f6783d3a0cb9c977692";
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+            appID,
+            serverSecret,
+            roomID,
+            Date.now().toString(),
+            "Mahika"
+        );
+
+        const zp = ZegoUIKitPrebuilt.create(kitToken);
+
+        zp.joinRoom({
+            container: element,
+            sharedLinks: [
+                {
+                    name: 'Personal link',
+                    url:
+                        window.location.protocol + '//' +
+                        window.location.host + window.location.pathname +
+                        '?roomID=' +
+                        roomID,
+                },
+            ],
+            scenario: {
+                mode: ZegoUIKitPrebuilt.GroupCall,
+            },
+        });
+    }
+
+    return (
+        <div ref={meeting} style={{ width: '100vw', height: '100vh' }}>
+            <h1>Room</h1>
+        </div>
+    );
 }
